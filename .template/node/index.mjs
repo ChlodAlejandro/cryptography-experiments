cd30dd2417829d970d969f665d340343597756f2
Chlod Alejandro <chlod@chlod.net>
2022-11-09 15:31:41 +0800

    add template

---
+++ .template/node/index.mjs
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/**
+ * This file details the process of generating a PBKDF2-keyed and encrypted
+ * passphrase file for safely storing the passphrase of a given certificate.
+ */
+(async () => {
+
+})();
